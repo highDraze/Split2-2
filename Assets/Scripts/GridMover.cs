87802c767aa253925d584869e5dedf6419cb9285
Vadim Goryainov <v.goryainov@tum.de>
2023-06-10 17:25:22 +0200

    added grid mover

---
+++ Assets/Scripts/GridMover.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using DG.Tweening;
+
+public class GridMover : MonoBehaviour
+{
+    public float MoveDuration = 1f;
+
+    public delegate void DStartMove();
+    public static event DStartMove StartMove;
+    public delegate void DEndMove();
+    public static event DStartMove EndMove;
+
+    public enum Direction
+    {
+        UP,
+        RIGHT,
+        DOWN,
+        LEFT
+    }
+
+
+    public void MoveTiles(TileSelector[] tiles, Direction direction)
+    {
+        switch(direction)
+        {
+            case Direction.UP:
+                transform.DOMove(transform.position + new Vector3(0, 0, 1), MoveDuration);
+                StartCoroutine(MoveCoroutine());
+                break;
+            case Direction.RIGHT:
+                break;
+            case Direction.DOWN:
+                break;
+            case Direction.LEFT:
+                break;
+        }
+    }
+
+    IEnumerator MoveCoroutine()
+    {
+        StartMove?.Invoke();
+        yield return new WaitForSeconds(MoveDuration);
+        EndMove?.Invoke();
+    }
+}
