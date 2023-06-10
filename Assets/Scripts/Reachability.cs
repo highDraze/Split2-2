0c2912da5670d659972af68704ccbcbfa77e6dac
Vadim Goryainov <v.goryainov@tum.de>
2023-06-10 15:27:56 +0200

    added reachability

---
+++ Assets/Scripts/Reachability.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Reachability : MonoBehaviour
+{
+    public Transform target;
+
+    private bool _Reachable;
+    public bool Reachable
+    {
+        get
+        {
+            return _Reachable;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //GetComponent<NavMeshAgent>().destination = target.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        NavMeshPath path = new NavMeshPath();
+        NavMesh.CalculatePath(transform.position, target.position, NavMesh.AllAreas, path);
+
+        if (path.status == NavMeshPathStatus.PathComplete)
+        {
+            _Reachable = true;
+        }
+        else
+        {
+            _Reachable = false;
+        }
+    }
+}
