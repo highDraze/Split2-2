48e11eff4d90783a2ffc4d8a31bcf7640b164993
EliNeu <ely.neumann@gmx.de>
2023-06-10 15:35:06 +0200

    movement

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -7,10 +7,24 @@ public class Mover : MonoBehaviour
     [SerializeField]
     private float MoveSpeed = 3f;
 
+    [SerializeField]
+    private int playerIndex = 0;
+
     private CharacterController controller;
 
     private Vector3 moveDirection = Vector3.zero;
     private Vector2 inputVector = Vector2.zero;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    public int GetPlayerIndex()
+    {
+        return playerIndex;
+    }
+
     public void SetInputVector(Vector2 direction)
     {
         inputVector = direction;
@@ -22,6 +36,6 @@ public class Mover : MonoBehaviour
         moveDirection = transform.TransformDirection(moveDirection);
         moveDirection *= MoveSpeed;
 
-        controller.Move(moveDirection*Time.deltaTime);
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
--- Assets/Scripts/PlayerInputHandler.cs
+++ Assets/Scripts/PlayerInputHandler.cs
@@ -1,19 +1,27 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
 
 public class PlayerInputHandler : MonoBehaviour
 {
+    //private PlayerInput playerInput;
     private Mover mover;
     private void Awake()
     {
+        //playerInput = GetComponent<PlayerInput>();
         mover = GetComponent<Mover>();
+        //var movers = FindObjectsOfType<Mover>();
+        //var index = playerInput.playerIndex;
+        //mover = movers.FirstOrDefault(m => m.GetPlayerIndex() == index);
     }
-
+    
     public void OnMove(InputAction.CallbackContext context)
     {
+        //if(mover != null)
         mover.SetInputVector(context.ReadValue<Vector2>());
     }
+
 }
