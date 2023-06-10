e9ab718f4b1253658c0b77d867d6815bb87e3285
harukahikari <30721068+harukahikari@users.noreply.github.com>
2023-06-10 17:56:22 +0200

    TextMeshPro package

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")]
+    public Text
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
