ea976461e3d27da7a3cf7dcf90f04b6911241f46
harukahikari <30721068+harukahikari@users.noreply.github.com>
2023-06-10 20:45:28 +0200

    little timer.cs, TMPro package

---
+++ Assets/Scripts/GameLoop/Timer.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Component")] public TextMeshProUGUI timerText;
+
+    [Header("Timer Settings")] 
+    public float currentTime;
+    public bool countDown;
+  
+
+    // Update is called once per frame
+    void Update()
+    {
+        //currentTime = countDown ? currentTime -= Time.deltaTime : currentTime += Time.deltaTime;
+        currentTime -= Time.deltaTime;
+        timerText.text = currentTime.ToString();
+    }
+}
